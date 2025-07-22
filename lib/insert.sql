13f0b532e105351efd0a161c80e03b231fdb41e3
Mike <mclancy96@gmail.com>
2025-07-22 09:28:07 -0400

    Create and insert bears

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+  ('Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+  ('Tabitha', 5, 'F', 'Cinnamon', 'Gentle', 1),
+  ('Sergeant Brown', 12, 'M', 'Brown', 'Serious', 0),
+  ('Melissa', 4, 'F', 'Black', 'Playful', 1),
+  ('Grinch', 6, 'M', 'White', 'Grumpy', 0),
+  ('Wendy', 3, 'F', 'Brown', 'Friendly', 1),
+  (NULL, 8, 'M', 'Gray', 'Unknown', 0)
